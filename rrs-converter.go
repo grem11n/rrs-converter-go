78d0eb6ab8b782f43cca932ec5303836ca32d884
Yurii Rochniak <yurii.rochniak@tubemogul.com>
2016-05-18 14:39:27 +0300

    fix logging, updated gitignore

--- rrs-converter.go
+++ rrs-converter.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/user"
 	"sync"
 	"time"
@@ -12,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/fatih/color"
 	"gopkg.in/cheggaaa/pb.v1"
 )
 
@@ -33,12 +35,35 @@ var (
 	concurrencyPtr = flag.Int("maxcon", 10, "Set up maximum concurrency for this task. Default is 10")
 )
 
-func convert(attrs attrs) {
+func logger(bucket string, info map[string]error) {
+	f, err := os.Create(bucket + "-error.log")
+	if err != nil {
+		log.Println("Script ended with some errors, but log-file wasn't created due to: ", err)
+	}
+	defer f.Close()
+
+	logFile, err := os.OpenFile(bucket+"-error.log", os.O_WRONLY, 0666)
+	if err != nil {
+		log.Println("Script ended with some errors, but log-file wasn't written due to: ", err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+	for object, warning := range info {
+		log.Println("WARNING: Some issues occur while processing ", object, warning)
+	}
+	endMessg := "Script finished with some errors. Check " + bucket + "-error.log for details"
+	color.Red(endMessg)
+}
+
+func convert(attrs attrs) map[string]error {
+	warns := map[string]error{}
 	creds := credentials.NewSharedCredentials(attrs.Config, attrs.Section)
 	_, err := creds.Get()
 	if err != nil {
+		color.Set(color.FgRed)
 		log.Fatal(err)
-		return
+		color.Unset()
 	}
 	// Create new connection to S3
 	svc := s3.New(session.New(), &aws.Config{
@@ -72,7 +97,7 @@ func convert(attrs attrs) {
 				}
 				_, err := svc.CopyObject(copyParams)
 				if err != nil {
-					log.Fatal(err)
+					warns[*content.Key] = err
 				}
 
 				<-throttle
@@ -86,6 +111,7 @@ func convert(attrs attrs) {
 	for i := 0; i < cap(throttle); i++ {
 		throttle <- 1
 	}
+	return warns
 }
 
 func main() {
@@ -94,9 +120,11 @@ func main() {
 	// Parsing arguments
 	flag.Parse()
 	if *bucketPtr == "" {
+		color.Set(color.FgRed)
 		fmt.Println("You haven't define bucket! Please, do it with -bucket= \n Script usage:")
 		flag.PrintDefaults()
 		log.Fatal("Bucket not specified")
+		color.Unset()
 		return
 	}
 	if *regionPtr == "" {
@@ -108,7 +136,9 @@ func main() {
 	if *configPtr == "" {
 		usr, err := user.Current()
 		if err != nil {
+			color.Set(color.FgRed)
 			log.Fatal(err)
+			color.Unset()
 			return
 		}
 		config = usr.HomeDir + "/.aws/credentials"
@@ -123,7 +153,10 @@ func main() {
 		Concurrency: *concurrencyPtr,
 	}
 
-	convert(attrs)
+	warns := convert(attrs)
+	if len(warns) > 0 {
+		logger(attrs.Bucket, warns)
+	}
 	elapsed := time.Since(start)
 	log.Printf("Convertion took: %s", elapsed)
 }
